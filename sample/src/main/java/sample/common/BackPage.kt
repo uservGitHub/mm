8d6268f7eefabe13c2594e84aa2a01e49a11f6a9
uservGitHub <userv@126.com>
2018-01-24 15:09:08 +0800

    BackPage 提供背景绘制

---
+++ sample/src/main/java/sample/common/BackPage.kt
@@ -0,0 +1,124 @@
+package sample.common
+
+import android.graphics.*
+import sample.utils.BmpUtils
+
+/**
+ * Created by work on 2018/1/24.
+ * 主要接口：
+ * endToEnd
+ * enable()
+ * setOnEndToEndListener
+ * draw([a,b], canvas)
+ */
+
+class BackPage(val count:Int = 50, val side:Int = 80) {
+    private var pages: List<Bitmap>
+    private var canUse: Boolean
+    init {
+        canUse = false
+        pages = List<Bitmap>(count, { index: Int ->
+            BmpUtils.buildBmp(80, index)
+        })
+    }
+
+    //region    pixs: [beg,end] 或 [-MAX, +MAX]
+    val beg: Int get() = if (endToEnd) Int.MIN_VALUE else 0
+    val end: Int get() = if (endToEnd) Int.MAX_VALUE else count * side - 1
+    //endregion
+    //region    能否响应endToEnd
+    fun enable(){
+        canUse = true
+    }
+    fun disable(){
+        canUse = false
+    }
+    //endregion
+
+    fun page(ind: Int) = pages[ind]
+
+    var endToEnd = false
+        set(value) {
+            if (field == value) return
+            field = value
+            if (canUse) {
+                onEndToEndListener?.let { it() }
+            }
+        }
+    //region    onEndToEndChanged
+    private var onEndToEndListener: (() -> Unit)? = null
+
+    fun setOnEndToEndListener(listener: () -> Unit) {
+        onEndToEndListener = listener
+    }
+    //endregion
+
+    /**
+     * pBeg为起点，对应canvas的canvasOffset点
+     * [pBeg, pEnd] 选择内容
+     * canvasOffsetPt 表示选择内容在canvas中的偏移起点，由外层调用canvas.transelate(pt)
+     */
+    fun draw(pBeg: Int, pEnd: Int, canvas: Canvas, canvasOffset: Point = Point(0, 0)) {
+        if (pBeg < end && beg < pEnd) {
+            val min = Math.max(pBeg, beg)
+            val max = Math.min(pEnd, end)
+            //region    兼容endToEnd
+            val minInd = getInd(min)
+            val maxInd = getInd(max)
+            val delta = getOffset(min)
+            canvas.translate(-delta.toFloat(), 0F)
+            //region    draw clipped pages
+            val pageRect = Rect(0,0,side,side)
+            var canvasRect = Rect(pageRect)
+            val paint = Paint().apply {
+                flags = Paint.ANTI_ALIAS_FLAG
+            }
+            val drawPage = { ind: Int ->
+                canvas.drawBitmap(pages[ind], pageRect, canvasRect, paint)
+                canvasRect.offset(side, 0)
+            }
+            if (minInd>maxInd){
+                for (ind in minInd until count){
+                    drawPage(ind)
+                }
+                for (ind in 0..maxInd){
+                    drawPage(ind)
+                }
+            }else{
+                for (ind in minInd..maxInd){
+                    drawPage(ind)
+                }
+            }
+            //endregion
+            canvas.translate(delta.toFloat(), 0F)
+            //endregion
+        }
+    }
+
+    /**
+     * ind 与 side 的关系 side = pages[ind]
+     * side 与 区间的关系 side = [0,side-1]
+     * x 正负都适用
+     * ind 大于等于0
+     * 不处理endToEnd
+     */
+    private inline fun getInd(x: Int): Int {
+        // T = [0, count*side-1] 或者 [0, count*side)
+        val T = count * side - 1
+        val ind = x.rem(T) / side
+        if (ind < 0) return ind + count
+        return ind
+    }
+
+    /**
+     * offset 与 side 的关系 offset 是在side上的截取起点
+     * offset 大于等于0 从左侧开始
+     * x 正负都适用
+     * 不处理endToEnd
+     */
+    private inline fun getOffset(x: Int): Int {
+        val offset = x.rem(side)
+        if (offset < 0) return offset + side
+        return offset
+    }
+}
--- sample/src/main/java/sample/utils/BmpUtils.kt
+++ sample/src/main/java/sample/utils/BmpUtils.kt
@@ -7,24 +7,31 @@ import android.graphics.*
  */
 class BmpUtils {
     companion object {
+
         fun buildBmp(side: Int, msg: Any? = null): Bitmap {
             val bmp = Bitmap.createBitmap(side, side, Bitmap.Config.RGB_565).apply {
                 eraseColor(Color.LTGRAY)
             }
+            val width = 3
+            //region pixRect
+            rectDraw(0, width, Color.BLACK, bmp)
+            rectDraw(width, width.shl(2), Color.RED, bmp)
+            //endregion
             val canvas = Canvas(bmp)
-            val strokeWidth = 3F
+
             val rect = Rect(0, 0, side, side)
+
             //region    rect,innerRect
+            /*val strokeWidth = width.toFloat()
             rectDraw(canvas,
                     strokePaint(Color.BLACK, 2 * strokeWidth),
                     rect)
-            val aliseStrokeWdith = strokeWidth.toInt()
 
-            val innerRect = Rect(aliseStrokeWdith, aliseStrokeWdith,
-                    side - aliseStrokeWdith, side - aliseStrokeWdith)
+            val innerRect = Rect(width, width,
+                    side - width, side - width)
             rectDraw(canvas,
                     strokePaint(Color.RED, 2 * strokeWidth),
-                    innerRect)
+                    innerRect)*/
             //endregion
             //region msg
             if (msg != null) {
@@ -60,6 +67,31 @@ class BmpUtils {
 
         private fun rectDraw(canvas: Canvas, strokePaint: Paint, rect: Rect) {
             canvas.drawRect(rect, strokePaint)
+        }
+
+        private fun rectDraw(offset:Int, width:Int, color:Int, bmp:Bitmap) {
+            val endX = bmp.width - offset - 1
+            val endY = bmp.height - offset - 1
+            for (x in offset until offset + width) {
+                for (y in offset..endY) {
+                    bmp.setPixel(x, y, color)
+                }
+            }
+            for (x in endX downTo endX - width + 1) {
+                for (y in offset..endY) {
+                    bmp.setPixel(x, y, color)
+                }
+            }
+            for (y in offset until offset + width) {
+                for (x in offset..endX) {
+                    bmp.setPixel(x, y, color)
+                }
+            }
+            for (y in endY downTo endY - width + 1) {
+                for (x in offset..endX) {
+                    bmp.setPixel(x, y, color)
+                }
+            }
         }
 
         private fun textDrawCenter(canvas: Canvas, textPaint: Paint, rect: Rect, msg: Any) {
